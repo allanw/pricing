2cb8d456deafbae5d1e754e3404c77ddd925ce0b
Allan Whatmough <al.whatmough@gmail.com>
2017-12-14 23:30:28 +0000

    Write orders data to csv file

--- main.go
+++ main.go
@@ -1,50 +1,50 @@
 package main
 
 import (
+	"encoding/csv"
+	"encoding/json"
 	"fmt"
-	"os"
-	"strconv"
 	"io/ioutil"
 	"net/http"
-	"encoding/json"
-	"encoding/csv"
+	"os"
+	"strconv"
 )
 
 type OrderItem struct {
-	ProductId	int	`json:"product_id"`
-	Quantity	int	
+	ProductId int `json:"product_id"`
+	Quantity  int
 }
 
 type Order struct {
 	Order struct {
-		Id	int
-		Items	[]OrderItem
+		Id    int
+		Items []OrderItem
 	}
 }
 
 type OrderItemInfo struct {
-	ProductId	int
-	Price		int
-	Vat		float32
-	Quantity	int
+	ProductId int
+	Price     int
+	Vat       float32
+	Quantity  int
 }
 
 type OrderInfo struct {
-	OrderId		int
-	TotalPrice	int
-	TotalVat	float32	
-	OrderItems	[]OrderItemInfo
+	OrderId    int
+	TotalPrice int
+	TotalVat   float32
+	OrderItems []OrderItemInfo
 }
 
 type Price struct {
-	ProductId	int	`json:"product_id"`
-	Price		int
-	VatBand		string	`json:"vat_band"`
+	ProductId int `json:"product_id"`
+	Price     int
+	VatBand   string `json:"vat_band"`
 }
 
 type PricingInfo struct {
-	Prices		[]Price
-	VatBands	map[string]float32	`json:"vat_bands"`
+	Prices   []Price
+	VatBands map[string]float32 `json:"vat_bands"`
 }
 
 func main() {
@@ -54,35 +54,55 @@ func main() {
 }
 
 func WriteOrderToCsvFile(order_info *OrderInfo, path string) {
-	file, err := os.Create(path)
+	_, err := os.Stat(path)
 	if err != nil {
-		panic(err)
+		// file doesn't exist, create it
+		file, err := os.Create(path)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		writer := csv.NewWriter(file)
+		defer writer.Flush()
+		writer.Write([]string{"order_id", "total_price", "total_vat", "product_id", "product_price", "quantity"})
+
+		for _, item := range order_info.OrderItems {
+			writer.Write([]string{strconv.Itoa(order_info.OrderId),
+				strconv.Itoa(order_info.TotalPrice),
+				strconv.FormatFloat(float64(order_info.TotalVat), 'f', 1, 32),
+				strconv.Itoa(item.ProductId),
+				strconv.Itoa(item.Price),
+				strconv.Itoa(item.Quantity)})
+		}
+	} else {
+		// file already exists
+		file, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0600)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		writer := csv.NewWriter(file)
+		defer writer.Flush()
+
+		for _, item := range order_info.OrderItems {
+			writer.Write([]string{strconv.Itoa(order_info.OrderId),
+				strconv.Itoa(order_info.TotalPrice),
+				strconv.FormatFloat(float64(order_info.TotalVat), 'f', 1, 32),
+				strconv.Itoa(item.ProductId),
+				strconv.Itoa(item.Price),
+				strconv.Itoa(item.Quantity)})
+		}
 	}
-	defer file.Close()
-
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	writer.Write([]string{"order_id","total_price","total_vat","product_id","product_price","quantity"})
-
-        for _, item:= range order_info.OrderItems {
-		writer.Write([]string{strconv.Itoa(order_info.OrderId),
-			     strconv.Itoa(order_info.TotalPrice),
-			     strconv.FormatFloat(float64(order_info.TotalVat), 'f', 1, 32),
-			     strconv.Itoa(item.ProductId),
-			     strconv.Itoa(item.Price),
-			     strconv.Itoa(item.Quantity)})
-        }
-
 }
 
 func order(w http.ResponseWriter, r *http.Request) {
-        body, err := ioutil.ReadAll(r.Body)
+	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		panic(err)
 	}
 	var order Order
-	err = json.Unmarshal(body, &order); if err != nil {
+	err = json.Unmarshal(body, &order)
+	if err != nil {
 		panic(err)
 	}
 
@@ -93,7 +113,7 @@ func order(w http.ResponseWriter, r *http.Request) {
 
 	price_map := make(map[int]Price)
 	vat_map := make(map[string]float32)
-	
+
 	for _, price := range pricing_data.Prices {
 		price_map[price.ProductId] = Price{Price: price.Price, VatBand: price.VatBand}
 	}
@@ -116,7 +136,7 @@ func order(w http.ResponseWriter, r *http.Request) {
 		total_order_vat += order_item_vat
 	}
 
-	order_info := OrderInfo{OrderId: order_id, TotalPrice: total_order_price, TotalVat: total_order_vat, OrderItems: order_items} 
+	order_info := OrderInfo{OrderId: order_id, TotalPrice: total_order_price, TotalVat: total_order_vat, OrderItems: order_items}
 
 	res, err := json.MarshalIndent(order_info, "", "\t")
 	if err != nil {
